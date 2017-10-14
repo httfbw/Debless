6526161ace37d2ea1aead3715655baa8475b43f3
Friedolin2000 <mail-fuer-friedolin@web.de>
2017-10-14 13:53:06 +0200

    CrazyLaberint Updata

--- code/src/httf/map/Maze.java
+++ code/src/httf/map/Maze.java
@@ -29,29 +29,32 @@ public class Maze {
 		draw(xPlayer, yPlayer, 0xFF0000);
 		System.out.println(xPlayer + " " + yPlayer);
 	}
+	
+	private int getRandom() {
+		if(new Random().nextBoolean()) {
+			return 0xFFFFFF;
+		}
+		return 0x000000;
+	}
 
 	private void generateCrazyLabyrinth() {
 		Random rand = new Random();
 		
 		for (int x = 0; x < level.pixels.length; x += 3) {
 			for (int y = 0; y < level.pixels[x].length; y += 3) {
-				level.pixels[x][y] = 0x0000FF;
-				level.pixels[x][y + 2] = 0x0000FF;
-				level.pixels[x + 2][y] = 0x0000FF;
-				level.pixels[x + 2][y + 2] = 0x0000FF;
-				level.pixels[x + 1][y + 1] = 0xFFFFFF;
+				level.pixels[x][y] = 0x000000;
+				level.pixels[x + 1][y] = getRandom();
+				level.pixels[x + 2][y] = 0x000000;
 				
-				if(rand.nextBoolean()) {
-					level.pixels[x + 1][y] = 0xFFFFFF;
-				} else {
-					level.pixels[x + 1][y] = 0x000000;
-				}
-				/*
-				if (rand.nextBoolean()) {
-					level.pixels[x][y] = 0x000000;
-				} else {
-					level.pixels[x][y] = 0xFFFFFF;
-				}*/
+				level.pixels[x][y + 1] = getRandom();
+				level.pixels[x + 1][y + 1] = 0xFFFFFF;
+				level.pixels[x + 2][y + 1] = getRandom();
+				
+				level.pixels[x][y + 2] = 0x000000;
+				level.pixels[x + 1][y + 2] = getRandom();
+				level.pixels[x + 2][y + 2] = 0x000000;
+				
+				
 
 			}
 		}
