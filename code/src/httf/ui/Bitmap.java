77f13825ce8df836b1eb2ca09b2bacd45bc4bccc
Christian Sohns <xenoblademc35@gmail.com>
2017-10-14 15:14:22 +0200

    maze

--- code/src/httf/ui/Bitmap.java
+++ code/src/httf/ui/Bitmap.java
@@ -15,7 +15,7 @@ public class Bitmap {
 	public void draw(Bitmap map, int xOffs, int yOffs) {
 		for(int x = xOffs; x < map.width + xOffs; x++) {
 			for(int y = yOffs; y < map.height + yOffs; y++) {
-				pixels[x][y] = map.pixels[x - xOffs][y - yOffs];
+				if(map.pixels[x - xOffs][y - yOffs] < 0) pixels[x][y] = map.pixels[x - xOffs][y - yOffs];
 			}
 		}
 	}
